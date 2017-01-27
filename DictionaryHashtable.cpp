476b010e50bcb22eccba96c71181ef6a7649ff1b
adrian cordova y quiroz <adriancordovayquiroz@adrians-MacBook-Pro-3.local>
2017-01-26 19:26:29 -0800

    hashtable complete

--- DictionaryHashtable.cpp
+++ DictionaryHashtable.cpp
@@ -3,19 +3,23 @@
 
 /* Create a new Dictionary that uses a Hashset back end */
 DictionaryHashtable::DictionaryHashtable(){
-	std::unordered_set<std::string> hashSet;
 }
 
 /* Insert a word into the dictionary. */
 bool DictionaryHashtable::insert(std::string word)
-{
+{	
+	//Initialize the pair that captures the return of insert
+	std::pair<std::unordered_set<std::string>::iterator,bool> success;
+
+	//Insert the word in the dictionary
 	hashSet.insert(word);
+	return success.second;
 }
 
 /* Return true if word is in the dictionary, and false otherwise */
 bool DictionaryHashtable::find(std::string word) const
 {
-	
+	return hashSet.find(word) != hashSet.end();
 }
 
 /* Destructor */
