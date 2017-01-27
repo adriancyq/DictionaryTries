f19a779b27d4106ad72b52e81d9268254e5751c0
adrian cordova y quiroz <adriancordovayquiroz@adrians-MacBook-Pro-3.local>
2017-01-26 18:57:51 -0800

    first hashtable implementation

--- DictionaryHashtable.cpp
+++ DictionaryHashtable.cpp
@@ -2,17 +2,23 @@
 #include "DictionaryHashtable.h"
 
 /* Create a new Dictionary that uses a Hashset back end */
-DictionaryHashtable::DictionaryHashtable(){}
+DictionaryHashtable::DictionaryHashtable(){
+	std::unordered_set<std::string> hashSet;
+}
 
 /* Insert a word into the dictionary. */
 bool DictionaryHashtable::insert(std::string word)
 {
+	hashSet.insert(word);
 }
 
 /* Return true if word is in the dictionary, and false otherwise */
 bool DictionaryHashtable::find(std::string word) const
 {
+	
 }
 
 /* Destructor */
-DictionaryHashtable::~DictionaryHashtable(){}
+DictionaryHashtable::~DictionaryHashtable(){
+	
+}
