908e6f5c6c95917a33659d994fad310b7bdd1666
adrian cordova y quiroz <adriancordovayquiroz@adrians-MacBook-Pro-3.local>
2017-02-09 17:43:26 -0800

    empty benchhash

---
+++ benchhash.cpp
@@ -0,0 +1,16 @@
+/*CSE 100 PA2 
+Adrian Cordova y Quiroz A12010305
+Jonathan Chiu PIDDDDDDDDDD */
+#include "DictionaryTrie.h"
+#include "DictionaryBST.h"
+#include "DictionaryHashtable.h"
+#include "util.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+int main (int argc, char* argv[]){
+	return 0;
+}
